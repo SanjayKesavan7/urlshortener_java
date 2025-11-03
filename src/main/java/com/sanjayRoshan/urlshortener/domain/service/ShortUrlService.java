93563599f569bc40f7d0ba2800793d314bdb992c
Sanjay Kesavan <kesavansanjay960@gmail.com>
2025-11-03 17:56:10 +0530

    added admin dashboard, my-urls page and method level security for admin

--- src/main/java/com/sanjayRoshan/urlshortener/domain/service/ShortUrlService.java
+++ src/main/java/com/sanjayRoshan/urlshortener/domain/service/ShortUrlService.java
@@ -44,11 +44,35 @@ public class ShortUrlService {
 
 
     public PagedResult<ShortUrlDto> findAllPublicShortUrls(int pageNo, int pageSize) {
-        pageNo = pageNo > 1? pageNo - 1 : 0;
-        Pageable pageable = PageRequest.of(pageNo, pageSize, Sort.by(Sort.Direction.DESC, "createdAt"));
+        Pageable pageable = getPageable(pageNo, pageSize);
         Page<ShortUrlDto> shortUrlDtoPage = shortUrlRepository.findPublicShortUrls(pageable)
                 .map(entityMapper::toShortUrlDto);
         return PagedResult.from(shortUrlDtoPage);
+    }
+
+    public PagedResult<ShortUrlDto> getUserShortUrls(Long userId, int page, int pageSize) {
+        Pageable pageable = getPageable(page, pageSize);
+        var shortUrlsPage = shortUrlRepository.findByCreatedById(userId, pageable)
+                .map(entityMapper::toShortUrlDto);
+        return PagedResult.from(shortUrlsPage);
+    }
+
+    @Transactional
+    public void deleteUserShortUrls(List<Long> ids, Long userId) {
+        if (ids != null && !ids.isEmpty() && userId != null) {
+            shortUrlRepository.deleteByIdInAndCreatedById(ids, userId);
+        }
+    }
+
+    public PagedResult<ShortUrlDto> findAllShortUrls(int page, int pageSize) {
+        Pageable pageable = getPageable(page, pageSize);
+        var shortUrlsPage =  shortUrlRepository.findAllShortUrls(pageable).map(entityMapper::toShortUrlDto);
+        return PagedResult.from(shortUrlsPage);
+    }
+
+    private Pageable getPageable(int page, int size) {
+        page = page > 1 ? page - 1: 0;
+        return PageRequest.of(page, size, Sort.Direction.DESC, "createdAt");
     }
     // validates and generates the short url for the original entered url
     @Transactional
